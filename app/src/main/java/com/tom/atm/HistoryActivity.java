1e35d17d29ec9d2fcefa7356650ad80943034b1b
Hank Thomas <tom@snpy.org>
2015-04-16 20:21:41 +0800

    20150413

--- app/src/main/java/com/tom/atm/HistoryActivity.java
+++ app/src/main/java/com/tom/atm/HistoryActivity.java
@@ -1,9 +1,20 @@
 package com.tom.atm;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class HistoryActivity extends Activity {
@@ -12,8 +23,44 @@ public class HistoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+        String userid = setting.getString("USERID", "");
+        String passwd = setting.getString("PASSWD", "");
+        new HistoryTask().execute("http://j.snpy.org/atm/h?userid="+userid+"&pw="+passwd);
+
     }
 
+    class HistoryTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String line = null;
+            StringBuffer data = new StringBuffer();
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                InputStream is = conn.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader in = new BufferedReader(isr);
+                line = in.readLine();
+                while(line!=null){
+                    data.append(line);
+                    line = in.readLine();
+                }
+                Log.d("NET", data.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data.toString();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
